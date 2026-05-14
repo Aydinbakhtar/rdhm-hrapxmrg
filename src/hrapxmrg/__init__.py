8ca1f03a09f2f5c70d775a1dab7d91b1fcbb9a29
aydin_aral <aydinbakhtar@gmail.com>
2026-05-14 07:34:40 -0500

    public helper added

--- src/hrapxmrg/__init__.py
+++ src/hrapxmrg/__init__.py
@@ -4,6 +4,8 @@ from .ascii_grid import AsciiGrid, read_ascii_grid
 from .hrap import HRAP_CRS_PROJ4, TargetGrid
 from .variables import VariableSpec, get_variable_spec
 from .xmrg import XMRGMeta, read_xmrg, write_xmrg
+from .pipeline import ascii_to_xmrg
+from .validate import ValidationResult, validate_xmrg_file
 
 __all__ = [
     "AsciiGrid",
@@ -15,4 +17,7 @@ __all__ = [
     "XMRGMeta",
     "read_xmrg",
     "write_xmrg",
+    "ascii_to_xmrg",
+    "ValidationResult",
+    "validate_xmrg_file"
 ]
